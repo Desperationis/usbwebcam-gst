182b13d8d834ca58b633bbbc93daa7ed803b3d41
Diego Contreras <smarttdiego@gmail.com>
2021-06-30 23:29:23 -0700

    Modify playWebcam.bash to receive parameters

--- playWebcam.bash
+++ playWebcam.bash
@@ -1,2 +1,2 @@
-gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1,width=1280,height=720 ! rtpjpegpay quality=100 ! udpsink host=192.168.1.189 port=8080
+gst-launch-1.0 v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1,width=1280,height=720 ! rtpjpegpay quality=100 ! udpsink host=${1} port=${2}
 
